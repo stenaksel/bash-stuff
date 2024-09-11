534ddac198422faf7914145bc279ce62cd04421f
StenAkselHeien <Sten.Aksel.Heien@gmail.com>
2024-09-11 14:46:16 +0200

    Updated aliases for Maven

--- .bashrc-incl.sh
+++ .bashrc-incl.sh
@@ -4,6 +4,14 @@
 # if [ -e $HOME/bash-stuff/.bashrc-incl.sh ]; then
 #     source $HOME/bash-stuff/.bashrc-incl.sh
 # fi
+ESC='\033'
+FG_RED=31
+BG_RED=41
+FG_CYAN=36
+CYAN="${ESC}[${FG_CYAN}m"
+RED="${ESC}[${FG_RED}m"
+ALERT="${ESC}[${BG_RED}m"
+NC="${ESC}[0m" # No Color
 
 printf "\n Installing \"bash-stuff\" (in .bashrc-incl.sh)\n"
 if [ -e $BASH_STUFF_PATH/.bash_aliases.sh ]; then
@@ -11,4 +19,21 @@ if [ -e $BASH_STUFF_PATH/.bash_aliases.sh ]; then
 fi
 if [ -e $BASH_STUFF_PATH/.bash_functions.sh ]; then
     source $BASH_STUFF_PATH/.bash_functions.sh
+fi
+# Add aliases and functions we would like to have
+#TODO: Split file, use includes... create one file for each language (& OS?)
+if [ -e $BASH_STUFF_PATH/.bash_win.sh ]; then
+    source $BASH_STUFF_PATH/.bash_win.sh
+fi
+if [ -e $BASH_STUFF_PATH/.bash_linux.sh ]; then
+    source $BASH_STUFF_PATH/.bash_linux.sh
+fi
+if [ -e $BASH_STUFF_PATH/.bash_mvn.sh ]; then
+    source $BASH_STUFF_PATH/.bash_mvn.sh
+fi
+if [ -e $BASH_STUFF_PATH/.bash_kt.sh ]; then
+    source $BASH_STUFF_PATH/.bash_kt.sh
+fi
+if [ -e $BASH_STUFF_PATH/.bash_py.sh ]; then
+    source $BASH_STUFF_PATH/.bash_py.sh
 fi
