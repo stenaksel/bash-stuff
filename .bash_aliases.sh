9eafc8f7e873e716a39967e659e8b3779af5628c
StenAkselHeien <Sten.Aksel.Heien@gmail.com>
2024-09-17 18:16:15 +0200

    gb-hist got alias gb-h

--- .bash_aliases.sh
+++ .bash_aliases.sh
@@ -131,7 +131,7 @@ alias gb='run git branch -a' # show branches (local and remote)
 alias gb-hist="for k in \$(git branch -r | perl -pe 's/^..(.*?)( ->.*)?\$/\1/');
               do echo -e \$(git show --pretty=format:\"%Cgreen%ci %Cblue%cr%Creset\" \$k -- | head -n 1)\\t\$k;
               done | sort -r"
-alias gbh='gb-hist'
+alias gb-h='gb-hist'
 #alias gb-hist2='for branch in \$(git for-each-ref --format='%(refname:short)' refs/heads/); do
 #                    echo -e \$(git log -1 --format="%ci" $branch)\\t$branch;' # show last update time on branches
 alias gbd='run git branch -d' # add <name> what branch to delete
